3578750f64009e523cf953d4b5f882151708ba1d
AMontalva <montalva@uci.edu>
2015-06-05 19:10:59 -0700

    css styling

--- add_todolist_item.php
+++ add_todolist_item.php
@@ -13,22 +13,7 @@ if (isset($_POST['submit'])) {
 	$description_post = $_POST["description"];
 	$todolist_item->add_todolist_item($name_post, $description_post);
 
-	// 2. Perform database query, assemble query, concatenate, easier to read
-	$query  = "SELECT * ";
-	$query .= "FROM todolist_item";
-	$result = $database->query($query);//send connection, and query, result is resource of database rows
-	$database->confirm_query($result);
-
-	// 3. Use returned data (if any)
-	while($subject = $database->fetch_assoc($result)) {
-	// output data from each row
-
-    ?>
-        <li><?php echo $subject["name"] . " (" . 
-        $subject["id"] . ")"; ?></li>
-      
-		<?php
-       }
+   	redirect_to("index.php");
 }
 else {
   // This is probably a GET request
--- index.php
+++ index.php
@@ -15,29 +15,55 @@ $database->confirm_query($result);
 <head>
 	<meta charset="UTF-8">
 	<title>To Do List</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<h1>To Do List</h1>
-	<form action="add_todolist_item.php" method="POST">
-		<input type="text" name="name" placeholder="Name">
-		<br><br>
-		<textarea name="description" id="" cols="30" rows="10" placeholder="Description"></textarea>
-		<br><br>
-		<input type="submit" name="submit">
-	</form>
-  	<ul>
+	<div id="form">
+		<h1>To Do List</h1>
+		<form action="add_todolist_item.php" method="POST">
+			<input type="text" name="name" placeholder="Name">
+			<br><br>
+			<textarea name="description" id="" placeholder="Description"></textarea>
+			<br><br>
+			<button type="submit" name="submit">Submit</button>
+		</form>
+		<h2>User List</h2>
+	  	<ul>
+			<?php
+			// 3. Use returned data (if any)
+			while($subject = $database->fetch_assoc($result)) {
+			// output data from each row
+
+		    ?>
+	            <li><?php echo $subject["name"] . " (" . 
+	            $subject["id"] . ")"; ?></li>
+	          
+	   		<?php
+	        }
+	      	?>
+		</ul>
+		<h2>Task Item List</h2>
+		<?php
+		// 2. Perform database query, assemble query, concatenate, easier to read
+		$query  = "SELECT * ";
+		$query .= "FROM todolist_item";
+		$result = $database->query($query);//send connection, and query, result is resource of database rows
+		$database->confirm_query($result);
+		?>
+		<ul>
 		<?php
 		// 3. Use returned data (if any)
 		while($subject = $database->fetch_assoc($result)) {
 		// output data from each row
 
 	    ?>
-            <li><?php echo $subject["name"] . " (" . 
-            $subject["id"] . ")"; ?></li>
-          
-   		<?php
-           }
-      	?>
-	</ul>
+	        <li><?php echo $subject["name"] . " (" . 
+	        $subject["id"] . ")"; ?></li>
+	      
+		<?php
+	    }
+	    ?>
+	    </ul>
+	</div>
 </body>
 </html>
